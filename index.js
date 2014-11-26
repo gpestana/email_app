8e344333f49925a60497a410562c1f096243d430
gpestana <g6pestana@gmail.com>
2014-11-26 16:52:43 +0100

    pdf & mail. flow

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-var email = require('./lib/smtp.js'),
-pdf       = require('./lib/pdf_converter.js')
+var express = require('express'), 
+http        = require('http'),
+email       = require('./lib/smtp.js'),
+pdf         = require('./lib/pdf_converter.js')
 
-//convert html to pdf
-var url = "http://google.com"
-pdf.convert(url)
+var app = express()
+var port = 3030
 
-//send email
-var attachment = {path:"data/test.pdf", type:"pdf", name:"testing_pdf"}
-//email.send(attachment)
+app.listen(process.env.PORT || port, function(){
+  console.log('server started')
+})
+
+app.get('/send', function(req, res) {
+
+  //#TODO: sanitize input
+  var mail_dest = req.query.mail
+  var url = req.query.url
+
+
+  var ts = new Date().getTime()
+  var file_name = url+"_"+ts+".pdf"
+
+  console.log("1. processing "+file_name)
+
+  pdf.convert(url, file_name, function() {
+
+    var attachment = {
+      path:'data/'+file_name, 
+      type:"pdf", name: file_name
+    }
+
+    email.send(mail_dest, attachment, function(err, msg) {
+      console.log(err || "status: "+file_name+" sent to "+mail_dest)
+
+      if(err) res.send(err.toString())
+      else res.send(msg)
+    })
+  })
+
+})
+
