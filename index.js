cd4561494d9393184c3de8cdebad4852b8de77e6
gpestana <g6pestana@gmail.com>
2014-11-27 11:14:16 +0100

    imap hook

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 var express = require('express'), 
 http        = require('http'),
 email       = require('./lib/smtp.js'),
-pdf         = require('./lib/pdf_converter.js')
+pdf         = require('./lib/pdf_converter.js'),
+hook        = require('./lib/hook.js')
 
 var app = express()
 var port = 3030
 
 app.listen(process.env.PORT || port, function(){
-  console.log('server started')
+  console.log('[main] server started')
 })
 
 app.get('/send', function(req, res) {
-
   //#TODO: sanitize input
   var mail_dest = req.query.mail
   var url = req.query.url
-
 
   var ts = new Date().getTime()
   var file_name = url+"_"+ts+".pdf"
 
   console.log("1. processing "+file_name)
-
   pdf.convert(url, file_name, function() {
 
     var attachment = {
-      path:'data/'+file_name, 
+      path:'./data/'+file_name, 
       type:"pdf", name: file_name
     }
 
@@ -39,3 +37,26 @@ app.get('/send', function(req, res) {
 
 })
 
+
+app.get('/hook', function(req, res) {
+  res.send('web interface no used anymore')
+})
+
+
+var start = function() {
+  var curr_seqno = 99992
+
+  console.log("[main] starting")
+
+  hook.connect(function(data, seqno) {
+    var new_msgs = seqno-curr_seqno
+    if(new_msgs>0) {
+      console.log(new_msgs)
+    } else console.log("[main] no new msgs")
+  })
+}
+
+
+
+//main
+start()
