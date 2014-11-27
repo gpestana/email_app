c100dec5750bbe1976256b68a9a084bc744d74a0
gpestana <g6pestana@gmail.com>
2014-11-27 16:45:07 +0100

    v0.6

--- index.js
+++ index.js
@@ -1,62 +1,44 @@
-var express = require('express'), 
-http        = require('http'),
-email       = require('./lib/smtp.js'),
+var email   = require('./lib/smtp.js'),
 pdf         = require('./lib/pdf_converter.js'),
-hook        = require('./lib/hook.js')
+hook        = require('./lib/hook.js'),
+disk        = require('./lib/disk.js')
 
-var app = express()
-var port = 3030
+var msg_bounds = "HTML>"
 
-app.listen(process.env.PORT || port, function(){
-  console.log('[main] server started')
-})
 
-app.get('/send', function(req, res) {
-  //#TODO: sanitize input
-  var mail_dest = req.query.mail
-  var url = req.query.url
+var app = function() {
+  console.log("[main] starting ")
 
-  var ts = new Date().getTime()
-  var file_name = url+"_"+ts+".pdf"
+  hook.connect(function(raw_data) {
+    var data = raw_data.split(msg_bounds)
 
-  console.log("1. processing "+file_name)
-  pdf.convert(url, file_name, function() {
+    //console.log(data)
 
-    var attachment = {
-      path:'./data/'+file_name, 
-      type:"pdf", name: file_name
+    if(data[1]) {
+      console.log("[main] incoming data ")
+ 
+      disk.write(data[1], function(filename) {
+
+        pdf.convertPDF(filename, function(err){
+          if(err) console.log("[main] err @ converterPDF")
+          else {
+            var attachment = {
+              path:"data/pdf/"+filename+".pdf",
+              type:"pdf", 
+              name: "attachment.pdf"
+              }
+ 
+            email.send("g6pestana@gmail.com", attachment, 
+              function(err, msg){
+                if(err) console.log("[main] err @ email: "+err)
+                else console.log("[main] DONE") 
+              })  
+          }
+        }) 
+      })  
     }
-
-    email.send(mail_dest, attachment, function(err, msg) {
-      console.log(err || "status: "+file_name+" sent to "+mail_dest)
-
-      if(err) res.send(err.toString())
-      else res.send(msg)
-    })
-  })
-
-})
-
-
-app.get('/hook', function(req, res) {
-  res.send('web interface no used anymore')
-})
-
-
-var start = function() {
-  var curr_seqno = 99992
-
-  console.log("[main] starting")
-
-  hook.connect(function(data, seqno) {
-    var new_msgs = seqno-curr_seqno
-    if(new_msgs>0) {
-      console.log(new_msgs)
-    } else console.log("[main] no new msgs")
   })
 }
 
 
-
-//main
-start()
+app()
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+http        = require('http'),
+email       = require('./lib/smtp.js'),
+pdf         = require('./lib/pdf_converter.js'),
+hook        = require('./lib/hook.js')
+
+var app = express()
+var port = 3030
+
+
+app.listen(process.env.PORT || port, function(){
+  console.log('[main] server started')
+})
+
+
+app.get('/send', function(req, res) {
+  //#TODO: sanitize input
+  var mail_dest = req.query.mail
+  var url = req.query.url
+
+  var ts = new Date().getTime()
+  var file_name = url+"_"+ts+".pdf"
+
+  console.log("1. processing "+file_name)
+  pdf.convert(url, file_name, function() {
+    var attachment = {
+      path:'./data/'+file_name,
+      type:"pdf", name: file_name
+    }
+    email.send(mail_dest, attachment, function(err, msg) {
+      console.log(err || "status: "+file_name+" sent to "+mail_dest)
+      if(err) res.send(err.toString())
+      else res.send(msg)
+    })
+  })
+
+})
+
+
+app.get('/hook', function(req, res) {
+  res.send('web interface no used anymore')
+})
+
