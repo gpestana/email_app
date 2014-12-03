90f8fda2c5aad13ab02a87da02816118db61ea29
gpestana <g6pestana@gmail.com>
2014-12-03 09:31:52 +0100

    configs refactor

---
+++ configs/config.js
@@ -0,0 +1,7 @@
+var sender_mail = "g6pestana@gmail.com",
+sender_name     = "Orders Catchbox",
+bcc_mail        = "orders@thecatchbox.com"
+
+exports.sender_mail = sender_mail
+exports.sender_name = sender_name
+//exports.bbc_mail    = bbc_mail
